910cdde7548ac4524c15664c72b2a142fc43a21f
Administrator@JCRP.LOCAL <Administrator@JCRP.LOCAL>
2018-09-29 19:47:53 -0500

    changed 'Playing Game' to 'jcrpweb.com'

--- Development/index.js
+++ Development/index.js
@@ -24,7 +24,7 @@ fs.readdir("./commands/", (err, files) => {
 // Displays the message in console
 bot.on("ready", async () => {
     
-    // bot.user.setActivity(".", {type: "PLAYING"});
+    bot.user.setActivity("jcrpweb.com", {type: "PLAYING"});
     // console.log('\x1b[92m', `${bot.user.username} is now online.\n ${bot.user.username} is now active on ${bot.guilds.size} guilds.`);
     console.log(`Bot online and currently serving in ${bot.channels.size} channels on ${bot.guilds.size} servers, for a total of ${bot.users.size} users.`)
 
