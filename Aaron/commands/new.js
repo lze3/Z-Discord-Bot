ba964f28df20f770ade898357c6494d2867bcf49
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-03 12:26:28 +0000

    removed the dumb and unnecessary bots that we had. (Aaron and Tim)

--- Aaron/commands/new.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports.run = async(bot, message) => {
-    message.channel.send('hi')
-}
-
-module.exports.help = {
-    name: 'hi'
-}
--- Tim/commands/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports.run = async (bot, message, args) => {
-    return message.channel.send(`Pong! \`${new Date().getTime() - message.createdTimestamp}ms\``);
-}
-
-module.exports.help = {
-    name: "ping"
-}
