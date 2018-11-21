643e0ab294dfb019b1269a6853251f56e5392977
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-21 20:11:15 +0000

    updated code - instead of checking if it !equal, check if it does equal

--- Development/commands/app_acc.js
+++ Development/commands/app_acc.js
@@ -1,42 +1,88 @@
+const Discord = require('discord.js')
+
+let config = require('../botconfig.json')
+let prefix = config.prefix
+
+const main_discord_info = 
+{
+    ["id"] : '354062777737936896',
+    ["name"] : 'JusticeCommunityRP' // might change
+}
+
+
 module.exports.run = async(bot, message, args) => {
-    let applicant = message.mentions.users.first()
-    if(!(message.member.roles.has('481541340337930269') || message.guild.name === 'JusticeCommunityRP - Development'))
+    let applicant = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) // target
+    if(message.member.roles.find('name', 'Director' || 'The Directorate') || message.guild.name === 'JusticeCommunityRP - Development')
+    {
+        if(applicant)
+        {
+            if(args[1] === 'sahp')
+            {
+                let embed = new Discord.RichEmbed()
+                .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
+                .setDescription(applicant + "'s application for the San Andreas Highway Patrol has been accepted!")
+                .setFooter("SAHP Department FTO", "https://i.imgur.com/mVLhtCc.png")
+                .setTimestamp()
+                .setColor("#c2f1ff")
+
+                message.channel.send(embed)
+
+                if(message.guild.id === main_discord_info.id)
+                {
+                    applicant.addRole('354074453270986752')
+                    applicant.addRole('354074696322514950')
+                }
+            }
+            else if (args[1] === 'lssd')
+            {
+                let embed = new Discord.RichEmbed()
+                .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
+                .setDescription(applicant + "'s application for the Los Santos Sheriff's Department has been accepted!")
+                .setFooter("LSSD Department FTO", "https://i.imgur.com/tLIylue.png")
+                .setTimestamp()
+                .setColor("#c2f1ff")
+
+                message.channel.send(embed)
+
+                if(message.guild.id === main_discord_info.id)
+                {
+                    applicant.addRole('354073428795588618') // LSSD
+                    applicant.addRole('354074696322514950')
+                }
+            }
+            else if (args[1] === 'lspd')
+            {
+                let embed = new Discord.RichEmbed()
+                .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
+                .setDescription(applicant + "'s application for the Los Santos Police Department has been accepted!")
+                .setFooter("LSPD Department FTO", "https://i.imgur.com/TRW0Nu0.png")
+                .setTimestamp()
+                .setColor("#c2f1ff")
+
+                message.channel.send(embed)
+
+                if(message.guild.id === main_discord_info.id)
+                {
+                    applicant.addRole('505871307041406997') // LSSD
+                    applicant.addRole('354074696322514950')
+                }
+            }
+            else
+            {
+                message.channel.send(message.author + ", improper usage. " + prefix + module.exports.help.name + " user dept")
+            }
+        }
+        else
+        {
+            message.channel.send(message.author + ", I could not find that user.")
+        }
+    }
+    else
     {
         message.channel.send(message.author + ", you do not have permission to use that command.")
     }
-    else
-    if(!applicant)
-    {
-        message.channel.send(message.author + ", could not find user.")
-    }
-    else
-    {
-        if(args[1] === 'sahp')
-        {
-            message.channel.send(applicant + "'s application for the San Andreas Highway Patrol has been accepted!")
-            applicant.addRole('354074453270986752') // Train Me (SAHP)
-            applicant.addRole('354074696322514950') // Interview Me (Application Accepted)
-        }
-        else
-        if (args[1] === 'lssd')
-        {
-            message.channel.send(applicant + "'s application for the Los Santos Sheriff's Department has been accepted!")
-            applicant.addRole('354073428795588618')
-            applicant.addRole('354074696322514950')
 
-        }
-        else
-        if (args[1] === 'lspd')
-        {
-            message.channel.send(applicant + "'s application for the Los Santos Police Department has been accepted!")
-            applicant.addRole('505871307041406997')
-            applicant.addRole('354074696322514950')
-        }
-        else
-        {
-            message.channel.send(message.author + ", `;" + module.exports.help.name + " user dept`")
-        }
-    }
+    message.delete().catch(O_o => {}); // deletes the message
 }
 
 module.exports.help = {
