0f9bae7e3b8882ce1faa9cb63d2cfc1cb4cef8f5
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-23 20:35:51 +0000

    updated command

--- Development/commands/addrole.js
+++ Development/commands/addrole.js
@@ -1,26 +1,63 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-//!addrole @andrew Dog Person
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
-  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user, yo.");
-  let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
-  let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+	let user_t = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	let role_c = args.join(" ").slice(22)
+	let role_s = message.guild.roles.find('name', role_c)
+	if(message.member.roles.find('name', "Director") || message.guild.name === 'JusticeCommunityRP - Development')
+	{
+		if(user_t)
+		{
+			if(role_s)
+			{
+				if(!user_t.roles.has(role_s.id))
+				{
+					message.channel.send(user_t + " has been given the `" + role_c + "` role.")
+					await(user_t.addRole(role_s.id))
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
-  await(rMember.addRole(gRole.id));
+					try
+					{
+						await user_t.send("You have been given the `" + role_c + "` role.")
+					}
+					catch(e)
+					{
+						console.error("Tried DMing user [ " + user_t.username + " ]. Failed because user's DMs are disabled.")
+						message.channel.send("Tried DMing user **" + user_t.nickname + "**. Failed as the user's DMs are disabled.")
+					}
+				}
+				else
+				{
+					message.channel.send(user_t + " has been removed from the `" + role_c + "` role.")
+					await(user_t.removeRole(role_s.id))
 
-  try{
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
-  }catch(e){
-    console.log(e.stack);
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
-  }
+					try
+					{
+						await user_t.send("You have been removed from the `" + role_c + "` role.")
+					}
+					catch(e)
+					{
+						console.error("Tried DMing user [ " + user_t.username + " ]. Failed because user's DMs are disabled.")
+						message.channel.send("Tried DMing user **" + user_t.nickname + "**. Failed as the user's DMs are disabled.")
+					}
+				}
+
+			}
+			else
+			{
+				message.channel.send(message.author + ", I could not find that role.")
+			}
+		}
+		else
+		{
+			message.channel.send(message.author + ", I could not find that user.")
+		}
+	}
+	else
+	{
+		message.channel.send(message.author + ", you do not have permission to use that command.")
+	}
 }
 
 module.exports.help = {
-    name: "addrole"
+	name: "addrole"
 }
