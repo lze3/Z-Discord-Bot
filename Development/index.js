085e6d7e54dc3cf2eac976e3a68f4a990fd5463a
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-24 13:29:03 +0000

    added tables for role ids + channel ids

--- Development/index.js
+++ Development/index.js
@@ -3,6 +3,31 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 
+global.roles = 
+{
+    "sahpdept" : "367796502824878080",
+    "sahpfto" : "354074444265947147",
+    "sahpsrank" : "354067549970890767",
+    "sahphighc" : "405444705560559618",
+
+    "lspddept" : "500799280974397463",
+    "lspdfto" : "505550111099584512",
+    "lspdsrank" : "500800444616933406",
+    "lspdhighc" : "500799280974397463",
+
+    "lssddept" : "367796714586898433",
+    "lssdfto" : "354074456940871681",
+    "lssdsrank" : "354074458618855444",
+    "lssdhighc" : "367796714586898433",
+
+    "safddept" : "367796857272926218",
+    "safdfto" : "354087776259538954",
+    "safdsrank" : "354086496996818946",
+    "safdhighc" : "405456643497918464"
+
+
+}
+
 // const errorchannel = member.guild.channels.find('name', 'jcrp-logs')
 bot.commands = new Discord.Collection(); 
 
