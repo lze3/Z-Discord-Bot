2311de280365d7b013751dcb1de48ea128c9c33c
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-25 03:10:38 +0000

    added purge

--- Development/commands/clear.js
+++ Development/commands/clear.js
@@ -1,9 +1,32 @@
 const Discord = require("discord.js")
 
+const config = require('../botconfig.json')
+const prefix = config.prefix
+
 module.exports.run = async (bot, message, args) => {
-  
+	if (message.member.roles.has('481541340337930269'))
+	{
+		if (args[0])
+		{
+            try {
+                await message.channel.bulkDelete(args[0])
+            } catch(error) {
+                message.channel.send(message.author + ", those messages are too old.")
+            }
+
+		}
+		else
+		{
+			message.channel.send(message.author + ", improper usage. `" + prefix + module.exports.help.name + " number`")
+		}
+	}
+	else
+	{
+		message.channel.send(message.author + ", you do not have permission to use that command.")
+		console.log(message.author.username + " entered a command that they do not have permission for [ " + module.exports.help.name + " ].")
+	}
 }
 
 module.exports.help = {
-    name: "clear"
+	name: "purge"
 }
