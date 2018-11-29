922d7f0832159031e38c5f037a4037d2054c8990
JHodgson1 <hodgsonjack@hotmail.com>
2018-11-29 22:08:43 +0000

    added note

--- Development/commands/co_channels.js
+++ Development/commands/co_channels.js
@@ -13,6 +13,8 @@ Channel name:
 Channel purpose:
 Proof of ownership:\`\`\`
     For 'proof of ownership' you can just provide a screenshot of you having the owner role for that organisation in the civilian operations Discord.
+
+    **NOTE:** Once your channel has been created your message will be deleted.
     `)
     .setFooter("Head of Civilian Operations", "https://i.imgur.com/ODI3OLT.png")
     .setColor('#9ae7ff')
