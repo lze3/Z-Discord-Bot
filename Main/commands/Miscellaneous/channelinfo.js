2df719a4ff9722b05acc1e3f16c388b88393bcf6
JHodgson1 <hodgsonjack@hotmail.com>
2018-12-08 13:28:11 +0000

    allow for searching voice channels via name without the need for the '▌' character.

--- Main/commands/Miscellaneous/channelinfo.js
+++ Main/commands/Miscellaneous/channelinfo.js
@@ -7,7 +7,7 @@ module.exports.run = async(bot, message, args) => {
     if (!args[0]) 
         channel = message.channel
     else
-        channel = message.mentions.channels.first() || message.guild.channels.find('name', args.join(" ")) || message.guild.channels.get(args[0])
+        channel = message.mentions.channels.first() || message.guild.channels.find('name', args.join(" ")) || message.guild.channels.find("name", `▌${args.join(" ")}`)
 
     if(!channel || channel === undefined || channel === null) return message.reply("I couldn't find that channel.")
     let topic = channel.topic ? channel.topic : "None"
