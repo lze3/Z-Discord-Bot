d473f6dd1b9f7c5c450035a5373320a3849b312f
JHodgson1 <hodgsonjack@hotmail.com>
2019-01-02 04:53:23 +0000

    temp command fix for .dept (dev)

--- Development/commands/Department/app_acc.js
+++ Development/commands/Department/app_acc.js
@@ -16,76 +16,40 @@ let dev =
 }
 
 module.exports.run = async(bot, message, args) => {
-    let applicant = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]) // target
-    if(message.member.roles.find('name', 'Director' || 'The Directorate') || message.guild.name === dev.name)
+    let applicant = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+
+    if(!message.member.roles.has('481541340337930269')) return message.channel.reply("you are not authorized to use that command.")
+    if (!args) return message.reply("you need to provide some arguments.")
+    if (!applicant) return message.reply("I could not find that user.")
+    if (!args[1]) return message.reply("you need to specify a department.")
+    // if (args[1] !== "sahp" || args[1] !== "lssd" || args[1] !== "lspd") return message.reply("that is an invalid department.")
+    if (args[1] === "sahp")
     {
-        if(applicant)
-        {
-            if(args[1] === 'sahp')
-            {
-                let embed = new Discord.RichEmbed()
-                .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
-                .setDescription(applicant + "'s application for the San Andreas Highway Patrol has been accepted!")
-                .setFooter("SAHP Department FTO", "https://i.imgur.com/mVLhtCc.png")
-                .setTimestamp()
-                .setColor("#c2f1ff")
-
-                message.channel.send(embed)
-
-                if(message.guild.id === main.id)
-                {
-                    applicant.addRole('354074453270986752')
-                    applicant.addRole('354074696322514950')
-                }
-            }
-            else if (args[1] === 'lssd')
-            {
-                let embed = new Discord.RichEmbed()
-                .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
-                .setDescription(applicant + "'s application for the Los Santos Sheriff's Department has been accepted!")
-                .setFooter("LSSD Department FTO", "https://i.imgur.com/tLIylue.png")
-                .setTimestamp()
-                .setColor("#c2f1ff")
-
-                message.channel.send(embed)
-
-                if(message.guild.id === main.id)
-                {
-                    applicant.addRole('354073428795588618') // LSSD
-                    applicant.addRole('354074696322514950')
-                }
-            }
-            else if (args[1] === 'lspd')
-            {
-                let embed = new Discord.RichEmbed()
-                .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
-                .setDescription(applicant + "'s application for the Los Santos Police Department has been accepted!")
-                .setFooter("LSPD Department FTO", "https://i.imgur.com/TRW0Nu0.png")
-                .setTimestamp()
-                .setColor("#c2f1ff")
-
-                message.channel.send(embed)
-
-                if(message.guild.id === main.id)
-                {
-                    applicant.addRole('505871307041406997') // LSSD
-                    applicant.addRole('354074696322514950')
-                }
-            }
-            else
-            {
-                message.channel.send(message.author + ", improper usage. `" + prefix + module.exports.help.name + " user dept`.")
-            }
-        }
-        else
-        {
-            message.channel.send(message.author + ", I could not find that user.")
-        }
+        description = applicant + "'s application for the San Andreas Highway Patrol has been accepted! Please contact an FTO to arrange your interview and training."
+        footer = "SAHP Department FTO"
+        foot_img = "https://i.imgur.com/mVLhtCc.png"
     }
-    else
+    else if (args[1] === "lssd")
     {
-        message.channel.send(message.author + ", you do not have permission to use that command.")
+        description = applicant + "'s application for the Los Santos Sheriff's Department has been accepted! Please contact an FTO to arrange your interview and training."
+        footer = "LSSD Department FTO"
+        foot_img = "https://i.imgur.com/TRW0Nu0.png"
     }
+    else if (args[1] === 'lspd')
+    {
+        description = applicant + "'s application for the Los Santos Police Department has been accepted! Please contact an FTO to arrange your interview and training."
+        footer = "LSPD Department FTO"
+        foot_img = "https://i.imgur.com/tLIylue.png"
+    }
+
+    let embed = new Discord.RichEmbed()
+    .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
+    .setDescription(description)
+    .setFooter(footer, foot_img)
+    .setTimestamp()
+    .setColor('#c2f1ff')
+
+    message.channel.send(embed)
 
     message.delete().catch(O_o => {}); // deletes the message
 }
