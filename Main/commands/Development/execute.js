66269e46322e4b33ad97155f28842b075cc77f40
JusticeCommunityRP <hodgsonjack@hotmail.com>
2019-01-16 22:26:40 +0100

    allow internal admin to use .exec

--- Main/commands/Development/execute.js
+++ Main/commands/Development/execute.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(bot, message, args) => {
-    if(!message.member.roles.has('481541340337930269')) return console.log("User tried executing command [ " + module.exports.help.name + " ].")
+    if(!message.member.roles.has('481541340337930269') || !message.member.roles.has('501076418399043604')) return console.log("User tried executing command [ " + module.exports.help.name + " ].")
     if(!args[0]) return;
     try {
-        var code = args.join(" ").replace(/\`\`\`js/, "")
+        var code = args.join(" ")
         eval(code)
     } catch(err) {
         const errembed = new Discord.RichEmbed()
