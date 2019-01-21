fcdca694ce059c6302e49632759790d27bec6013
JHodgson1 <hodgsonjack@hotmail.com>
2019-01-21 19:48:34 +0000

    add SACD to .dept

--- Main/commands/Department/dept.js
+++ Main/commands/Department/dept.js
@@ -10,7 +10,7 @@ module.exports.run = async(bot, message, args) => {
     let sahp_channel = message.guild.channels.find('name', 'jcrp-sahp')
     let lssd_channel = message.guild.channels.find('name', 'jcrp-lssd')
     let lspd_channel = message.guild.channels.find('name', 'jcrp-lspd')
-    let safd_channel = message.guild.channels.find('name', 'jcrp-safd')
+    let safd_channel = message.guild.channels.find('name', 'jcrp-safd')    
 
     if (!(message.member.roles.has('354093626592329739') || message.member.roles.has('481541340337930269'))) return message.reply("you are not allowed to use that command.")
     if (!applicant || applicant === null || applicant === undefined) return message.reply("I could not find that user.")
@@ -99,6 +99,26 @@ module.exports.run = async(bot, message, args) => {
         applicant.removeRole('354074696322514950')
         applicant.removeRole('354074696515584000')
 
+    } else if (dept.toUpperCase() === "SACD") 
+    {
+        let embed = new Discord.RichEmbed()
+        .setAuthor("FTO message from " + message.author.username, message.author.avatarURL)
+        .setDescription(applicant + " has passed their SACD training and has been given SACD department roles. Give them a warm welcome!")
+        .setFooter("SAFD Department FTO", "https://i.imgur.com/yekwbNs.png")
+        .setTimestamp()
+        .setColor("#f6a8ff")
+
+        safd_channel.send(embed)
+        message.reply("done.")
+
+        applicant.addRole('354095488666042380') // Law Enforcement
+        applicant.addRole('354095512225316876') // First Responders
+        applicant.addRole('367796857272926218') // Department
+        applicant.addRole('354086496996818946') // Department initiating rank (Officer/Cadet/Prob.)
+
+        applicant.removeRole('536989674560946186') // Train Me ()
+        applicant.removeRole('354074696322514950') 
+        applicant.removeRole('354074696515584000') 
     } else return message.reply("that is not a valid department.")
 
     message.delete().catch(O_o => {})
