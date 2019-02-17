b5a80a6bb2e51ad3cb7ee9f299bc99f40c43fcd4
JHodgson1 <hodgsonjack@hotmail.com>
2019-02-17 13:46:09 +0000

    properly handle errors

--- commands/Server/players.js
+++ commands/Server/players.js
@@ -35,23 +35,28 @@ module.exports.run = async (bot, message, args) => {
             if (err) return error(message.channel, `IP: ${IP}`)
             request.get(api1, {timeout: 2000}, function (err, response, body) {
                 if (err) return error(message.channel, `IP: ${IP}`)
+                    try {
 
-                var start = JSON.parse(body)
-                var start2 = JSON.parse(main)
-        
-                if (start == null || start == []) {
-                    var playersCount = 0
-                } else {
-                    var playersCount = start.length;
-                }
+                        var start = JSON.   parse(body)
+                        var start2 = JSON.parse(main)
+                
+                        if (start == null || start == []) {
+                            var playersCount = 0
+                        } else {
+                            var playersCount = start.length;
+                        }
 
-                var embed = new Discord.RichEmbed()
-                .setColor("#54C86D")
-                .setAuthor("Server Information" , `https://i.imgur.com/PnzJ35e.png`)
-                .setTitle(`Player count: ${playersCount}/${start2.vars.sv_maxClients}`)
-                .setDescription(start.map(element => '**' + element.name + "**  | ID: ``" + element.id + "``").join("\n\n"))
-                .setFooter(`Server IP: ${IP}`)
-                message.channel.send(embed);
+                        var embed = new Discord.RichEmbed()
+                        .setColor("#54C86D")
+                        .setAuthor("Server Information" , `https://i.imgur.com/PnzJ35e.png`)
+                        .setTitle(`Player count: ${playersCount}/${start2.vars.sv_maxClients}`)
+                        .setDescription(start.map(element => '**' + element.name + "**  | ID: ``" + element.id + "``").join("\n\n"))
+                        .setFooter(`Server IP: ${IP}`)
+                        message.channel.send(embed);
+                    } catch (err) {
+                        error(message.channel, `Server IP: ${IP}`)
+                    }
+
             
 
             })
