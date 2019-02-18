03a54ef6e820832cf8b142e46a5e713a534e5a46
JHodgson1 <hodgsonjack@hotmail.com>
2019-02-18 23:39:35 +0000

    add rcon command

---
+++ commands/Server/rcon.js
@@ -0,0 +1,38 @@
+const Q3RCon = require('quake3-rcon')
+const RConCFG = require('../../rcon_info.json')
+
+module.exports.run = async(bot, message, args) => {
+    if (!(message.member.roles.has('546360545133985793') || message.member.roles.has('541031422174167105'))) { return message.reply('you cannot use that command.').then(() => console.log(`${message.author} tried executing .exec`))}
+    var server = args[0]
+    if (!server) return message.reply("you need to specify a server to execute commands to. (1s or reg)")
+    var command = args[1]
+    if (!command) return message.reply("you need to specify a command to execute.")
+    var params = args.join(" ").slice(server.length+command.length+1)
+    var RegRCon = new Q3RCon({
+        address: RConCFG.IP,
+        port: RConCFG.Regular.Port,
+        password: RConCFG.Password,
+        debug: false
+    })
+    var OneSyncRCon = new Q3RCon({
+        address: RConCFG.IP,
+        port: RConCFG.OneSync.Port,
+        password: RConCFG.Password,
+        debug: false
+    })
+    if (server.toUpperCase() === "REGULAR" || server.toUpperCase() === "REG") {
+        RegRCon.send(`${command} ${params}`, function(response) {
+            message.reply(response.slice(6))
+        })
+    } else if (server.toUpperCase() === "ONESYNC" || server.toUpperCase() === "OS" || server.toUpperCase() === "1S") {
+        OneSyncRCon.send(`${command} ${params}`, function(response) {
+            message.reply(response.slice(6))
+        })
+    } else return message.reply("invalid server.")
+
+    
+}
+
+module.exports.help = {
+    name: 'rcon'
+}
