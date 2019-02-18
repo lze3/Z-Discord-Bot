374782eeb24a9cebbf1a27390fdfc0a8b8ba696c
JHodgson1 <hodgsonjack@hotmail.com>
2019-02-18 22:21:01 +0000

    change apic command usage, add ability to check through resources

--- commands/Server/apic.js
+++ commands/Server/apic.js
@@ -4,31 +4,53 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
     const _api = "https://servers-live.fivem.net/api/servers/";
-    let _includes = args.join(" ")
+    let _includes = args.join(" ").slice(args[0].length + 1)
 
-    request({
-        uri: _api,
-        json: true
-    }, (error, response, body) => {
-        let Servers = ""
-        for (let server of body)
-            if (server.Data.hostname.includes(_includes) || server.Data.hostname.includes(_includes.toUpperCase())) {
-                Servers += server.EndPoint + " - " +  server.Data.hostname + "\n"
-            }
+    if (args[0] === 'hostname') {
+        request({            
+            uri: _api,
+            json: true
+        }, (error, response, body) => {
+            let Servers = ""
+            for (let server of body)
+                if (server.Data.hostname.includes(_includes) || server.Data.hostname.includes(_includes.toUpperCase())) {
+                    Servers += server.EndPoint + " - " +  server.Data.hostname + "\n"
+                }
 
-        let numb = Math.floor(Math.random() * 9999) + 1000
-        let numb1 = Math.floor(Math.random() * 9999) + 1000
-        let numb2 = Math.floor(Math.random() * 9999) + 1000
-        let numb3 = Math.floor(Math.random() * 9999) + 1000
-        fs.writeFile(`logs_\\${numb}-${numb1}-${numb2}-${numb3}.txt`, Servers, function(err) {
-            try {
-                message.channel.send("Search results:", {file: `logs_\\${numb}-${numb1}-${numb2}-${numb3}.txt`})
-            } catch(err) {
-                console.log(err)
-            }
+                let numb = Math.floor(Math.random() * 9999) + 1000
+                fs.writeFile(`logs_\\${_includes.toUpperCase()}-${numb}.txt`, Servers, function(err) {
+                    try {
+                        message.channel.send("Search results for hostname containing " + _includes + ":", {file: `logs_\\${_includes.toUpperCase()}-${numb}.txt`})
+                        console.log(_includes + " -> _includes")
+                        console.log(args[0] + " -> args[0]")
+                        console.log(args[1] + " -> args[1]")
+                    } catch(err) {
+                        console.log(err)
+                    }
+                })
+
         })
+    } else if (args[0] === 'resources') {
+        request({
+            uri: _api,
+            json: true
+        }, (error, response, body) => {
+            let Servers = ""
+            for (let server of body)
+                if (JSON.stringify(server.Data.resources).includes(_includes) || JSON.stringify(server.Data.resources).includes(_includes.toUpperCase())) {
+                    Servers += server.EndPoint + " - " + server.Data.hostname + "\n"
+                }
 
-    })
+                let numb = Math.floor(Math.random() * 9999) + 1000
+                fs.writeFile(`logs_\\${_includes.toUpperCase()}-${numb}.txt`, Servers, function(err) {
+                    try {
+                        message.channel.send("Search results for resources containing " + _includes + ":", {file: `logs_\\${_includes.toUpperCase()}-${numb}.txt`})
+                    } catch(err) {
+                        console.log(err)
+                    }
+                })
+        })
+    }
 }
 
 module.exports.help = {
