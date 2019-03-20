08bc15f7072eaee4886cec847ab4e84022330cea
JHodgson1 <hodgsonjack@hotmail.com>
2019-03-20 22:44:41 +0000

    Disallow usage for anybody who doesn't have MANAGE_GUILD permission

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ bot.on("ready", async () => {
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
+	if (message.member.hasPermission("MANAGE_GUILD")) return;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
