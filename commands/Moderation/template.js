6a783c0d82f47bcb1c912304db023d1a3c5b1bad
JHodgson1 <hodgsonjack@hotmail.com>
2019-03-31 05:27:49 +0100

    cmds: add a template command for support (let people know what info to include)

---
+++ commands/Moderation/template.js
@@ -0,0 +1,22 @@
+module.exports.run = async(bot, message, args) => {
+    message.delete()
+    if (args[0] === "support") 
+    {
+        message.channel.send(
+        `:warning: __**Support Template**__ :warning:
+        
+        What is your issue?
+        When does the issue occur?
+        Have you installed it correctly?
+        Have you read the correct documentation in order to install any dependencies, if applicable?`
+        )
+    }
+    else 
+    {
+        message.author.send("Could not find template to use.")
+    }
+}
+
+module.exports.help = {
+    name: 'template'
+}
