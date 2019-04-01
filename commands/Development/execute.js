fdfa3cb7367043d8ac9c4958d16f2f5a0baf21c3
JHodgson1 <hodgsonjack@hotmail.com>
2019-04-01 23:24:41 +0100

    some altercations to .exec - restrict based on permission rather than role

--- commands/Development/execute.js
+++ commands/Development/execute.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(bot, message, args) => {
-    if(!(message.member.roles.has('546360545133985793') || message.member.roles.has('541031422174167105'))) { return message.reply('you cannot use that command.').then(() => console.log(`${message.author} tried executing .exec`))}
+    if(!message.member.hasPermission("ADMINISTRATOR")) { return message.reply('you cannot use that command.').then(() => console.log(`${message.author} tried executing .exec`))}
     if(!args[0]) return;
     try {
         var code = args.join(" ")
@@ -9,7 +9,7 @@ module.exports.run = async(bot, message, args) => {
     } catch(err) {
         const errembed = new Discord.RichEmbed()
         .setTitle("Error")
-        .setDescription(`\`\`${err.toString()}\`\``)
+        .setDescription(`\`${err.toString()}\``)
         .setColor('#F46E3F')
 
         message.channel.send(errembed)
