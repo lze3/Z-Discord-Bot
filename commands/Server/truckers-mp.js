f7a3253c0cd0cd20bc51ab2c0be0420048f863ab
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-08 01:15:13 +0100

    clean up code heavily - tmp

--- commands/Server/truckers-mp.js
+++ commands/Server/truckers-mp.js
@@ -45,14 +45,44 @@ module.exports.run = async(Client, message, args) => {
         // Instead of handling errors, just set these default values
         let GAME = args[1] ? args[1].toLowerCase() : "ETS2"
         let SERVER = args[2] ? args[2].toLowerCase() : "EU2"
+        if (GAME === "ets2") 
+        {
+            if (SERVER === "eu" || SERVER === "eu1")
+            {
+                server = "EU #1" // Use the information from that table
+            }
+            else if (SERVER === "eu2") 
+            {
+                server = "EU #2" 
+            }
+            else if (SERVER === "eu3")
+            {
+                server = "EU #3"
+            }
+            else if (SERVER === "eu4")
+            {
+                server = "EVENT"
+            }
+        }
+        else if (GAME === "ats")
+        {
+            if (SERVER === "us" || SERVER === "us1")
+            {
+                server = "US #1"
+            }
+            else if (SERVER === "eu")
+            {
+                server = "EU #2"
+            }
+        }
         request.get(`https://api.truckersmp.com/v2/servers/`, function(err, response, body) 
         {
             
             var main = JSON.parse(body);
             main.response.forEach(table => {
-                if (table.game.includes("ETS2") && table.shortname.includes("EU #1"))
+                if (table.game.includes(GAME) && table.shortname.includes(server))
                 {
-                    ETS2_EU1 = 
+                    server_info = 
                     {
                         ["id"]: table.id,
                         ["game"]: "Euro Truck Simulator 2",
@@ -70,141 +100,30 @@ module.exports.run = async(Client, message, args) => {
                         ["syncDelay"]: table.syncdelay
                     }  
                 }
-                else if (table.game.includes("ETS2") && table.shortname.includes("EU #2"))
+                else
                 {
-                    ETS2_EU2 = 
+                    if (table.game.includes("ETS2") && table.shortname.includes("EU #2"))
                     {
-                        ["id"]: table.id,
-                        ["game"]: "Euro Truck Simulator 2",
-                        ["ip"]: table.ip + ":" + table.port,   
-                        ["serverName"]: table.name,
-                        ["online"]: table.online ? "Yes" : "No :cry:",
-                        ["players"]: table.players,
-                        ["queue"]: table.queue == 0 ? "None." : table.queue,
-                        ["maxClients"]: table.maxplayers,
-                        ["speedLimiter"]: table.speedlimited == 1 ? "Enabled." : "Disabled.",
-                        ["collisions"]: table.collisions ? "Enabled." : "Disabled.",
-                        ["cars"]: table.carsforplayers ? "Enabled." : "Disabled.",
-                        ["policeCars"]: table.policecarsforplayers ? "Everybody can use." : "Only Game Moderators.",
-                        ["afkKick"]: table.afkenabled ? "Enabled." : "Disabled.",
-                        ["syncDelay"]: table.syncdelay
-                    }  
-                }
-                else if (table.game.includes("ETS2") && table.shortname.includes("EU #3"))
-                {
-                    ETS2_EU3 = 
-                    {
-                        ["id"]: table.id,
-                        ["game"]: "Euro Truck Simulator 2",
-                        ["ip"]: table.ip + ":" + table.port,   
-                        ["serverName"]: table.name,
-                        ["online"]: table.online ? "Yes" : "No :cry:",
-                        ["players"]: table.players,
-                        ["queue"]: table.queue == 0 ? "None." : table.queue,
-                        ["maxClients"]: table.maxplayers,
-                        ["speedLimiter"]: table.speedlimited == 1 ? "Enabled." : "Disabled.",
-                        ["collisions"]: table.collisions ? "Enabled." : "Disabled.",
-                        ["cars"]: table.carsforplayers ? "Enabled." : "Disabled.",
-                        ["policeCars"]: table.policecarsforplayers ? "Everybody can use." : "Only Game Moderators.",
-                        ["afkKick"]: table.afkenabled ? "Enabled." : "Disabled.",
-                        ["syncDelay"]: table.syncdelay
-                    }  
-                }
-                else if (table.game.includes("ETS2") && table.shortname.includes("EVENT"))
-                {
-                    ETS2_EU4 = 
-                    {
-                        ["id"]: table.id,
-                        ["game"]: "Euro Truck Simulator 2",
-                        ["ip"]: table.ip + ":" + table.port,   
-                        ["serverName"]: table.name,
-                        ["online"]: table.online ? "Yes" : "No :cry:",
-                        ["players"]: table.players,
-                        ["queue"]: table.queue == 0 ? "None." : table.queue,
-                        ["maxClients"]: table.maxplayers,
-                        ["speedLimiter"]: table.speedlimited == 1 ? "Enabled." : "Disabled.",
-                        ["collisions"]: table.collisions ? "Enabled." : "Disabled.",
-                        ["cars"]: table.carsforplayers ? "Enabled." : "Disabled.",
-                        ["policeCars"]: table.policecarsforplayers ? "Everybody can use." : "Only Game Moderators.",
-                        ["afkKick"]: table.afkenabled ? "Enabled." : "Disabled.",
-                        ["syncDelay"]: table.syncdelay
-                    }
-                }
-                else if (table.game.includes("ATS") && table.shortname.includes("US"))
-                {
-                    ATS_US1 =
-                    {
-                        ["id"]: table.id,
-                        ["game"]: "American Truck Simulator",
-                        ["ip"]: table.ip + ":" + table.port,   
-                        ["serverName"]: table.name,
-                        ["online"]: table.online ? "Yes" : "No :cry:",
-                        ["players"]: table.players,
-                        ["queue"]: table.queue == 0 ? "None." : table.queue,
-                        ["maxClients"]: table.maxplayers,
-                        ["speedLimiter"]: table.speedlimited == 1 ? "Enabled." : "Disabled.",
-                        ["collisions"]: table.collisions ? "Enabled." : "Disabled.",
-                        ["cars"]: table.carsforplayers ? "Enabled." : "Disabled.",
-                        ["policeCars"]: table.policecarsforplayers ? "Everybody can use." : "Only Game Moderators.",
-                        ["afkKick"]: table.afkenabled ? "Enabled." : "Disabled.",
-                        ["syncDelay"]: table.syncdelay
-                    }
-                }
-                else if (table.game.includes("ATS") && table.shortname.includes("EU #2"))
-                {
-                    ATS_EU2 =
-                    {
-                        ["id"]: table.id,
-                        ["game"]: "American Truck Simulator",
-                        ["ip"]: table.ip + ":" + table.port,   
-                        ["serverName"]: table.name,
-                        ["online"]: table.online ? "Yes" : "No :cry:",
-                        ["players"]: table.players,
-                        ["queue"]: table.queue == 0 ? "None." : table.queue,
-                        ["maxClients"]: table.maxplayers,
-                        ["speedLimiter"]: table.speedlimited == 1 ? "Enabled." : "Disabled.",
-                        ["collisions"]: table.collisions ? "Enabled." : "Disabled.",
-                        ["cars"]: table.carsforplayers ? "Enabled." : "Disabled.",
-                        ["policeCars"]: table.policecarsforplayers ? "Everybody can use." : "Only Game Moderators.",
-                        ["afkKick"]: table.afkenabled ? "Enabled." : "Disabled.",
-                        ["syncDelay"]: table.syncdelay
+                        server_info = 
+                        {
+                            ["id"]: table.id,
+                            ["game"]: "Euro Truck Simulator 2",
+                            ["ip"]: table.ip + ":" + table.port,   
+                            ["serverName"]: table.name,
+                            ["online"]: table.online ? "Yes" : "No :cry:",
+                            ["players"]: table.players,
+                            ["queue"]: table.queue == 0 ? "None." : table.queue,
+                            ["maxClients"]: table.maxplayers,
+                            ["speedLimiter"]: table.speedlimited == 1 ? "Enabled." : "Disabled.",
+                            ["collisions"]: table.collisions ? "Enabled." : "Disabled.",
+                            ["cars"]: table.carsforplayers ? "Enabled." : "Disabled.",
+                            ["policeCars"]: table.policecarsforplayers ? "Everybody can use." : "Only Game Moderators.",
+                            ["afkKick"]: table.afkenabled ? "Enabled." : "Disabled.",
+                            ["syncDelay"]: table.syncdelay
+                        }
                     }
                 }
             });
-            
-            server = ETS2_EU2 // This will be the default server
-
-            // Let's handle the correct information to display here...
-            if (GAME === "ets2") 
-            {
-                if (SERVER === "eu" || SERVER === "eu1")
-                {
-                    server = ETS2_EU1 // Use the information from that table
-                }
-                else if (SERVER === "eu2") 
-                {
-                    server = ETS2_EU2 
-                }
-                else if (SERVER === "eu3")
-                {
-                    server = ETS2_EU3
-                }
-                else if (SERVER === "eu4")
-                {
-                    server = ETS2_EU4
-                }
-            }
-            else if (GAME === "ats")
-            {
-                if (SERVER === "us" || SERVER === "us1")
-                {
-                    server = ATS_US1
-                }
-                else if (SERVER === "eu")
-                {
-                    server = ATS_EU2
-                }
-            }
 
             var embed = new Discord.RichEmbed()
             .setAuthor("TruckersMP Server Information", "https://w.wew.wtf/laugpu.png", "https://truckersmp.com")
