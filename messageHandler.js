b8c7372455441deb02e972792688740e0f9ee196
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-23 21:01:48 +0100

    fix indentation and remove message handler

--- messageHandler.js
+++
@@ -1,5 +0,0 @@
-Client.on("message", (message) => {
-    if (message.channel.name.includes("commands")) {
-        message.channel.bulkDelete(100).catch(err => console.log(err.toString()))
-    }
-})
