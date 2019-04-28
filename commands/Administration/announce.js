e5a6d22ab42fbe07ff9d54856ddffa4f7e0b8c8b
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-28 21:18:03 +0100

    add a fully working announce command

--- commands/Administration/announce.js
+++ commands/Administration/announce.js
@@ -1,18 +1,29 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(Client, message, args) => {
-    let title_delim = args.join(" ").split(/"(.+)"$/)
-    let regex = args.join(" ").split(/^"(.+)"$/)
-    if (!message.member.hasPermission("MANAGE_GUILD")) { message.author.send("You don't have permission to use that command."); return }
-    if (title_delim.length <= 0) { title = "Announcement" }
-    message.channel.send(`This will be the title:\n${title_delim[1]}`)
+    let deliminator = args.join(" ").split("\"") // title will embedded in quotes
 
-    var embed = new Discord.RichEmbed()
-    .setTitle(title_delim[1])
-    .setColor("#d91e18")
-    .setDescription(args.join(" ").slice(title_delim.length+2))
+    if (!args.join(" ").includes("\"")) { // if there isn't a title set using "title"
+        let body = args.join(" ")
+        const embed = new Discord.RichEmbed()
+        .setAuthor(`Announcement from ${message.member.user.username}`, message.author.avatarURL)
+        .setDescription(body)
+        .setColor("#F1383A")
+        .setTimestamp()
 
-    message.channel.send(embed)
+        message.channel.send(embed)
+    } else { // if there is a title set using "title"
+        let title = deliminator[1].replace("\"", "")
+        let body = args.join(" ").replace(`"${title}"`, "") // replace the title with an empty string...
+        const embed = new Discord.RichEmbed()
+        .setAuthor(`Announcement from ${message.member.user.username}`, message.author.avatarURL)
+        .setTitle(title.replace("\"", ""))
+        .setDescription(body)
+        .setColor("#F1383A")
+        .setTimestamp()
+
+        message.channel.send(embed)
+    }
 }
 
 module.exports.help = {
