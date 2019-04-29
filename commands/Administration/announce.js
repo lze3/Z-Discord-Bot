4b7601f6ff016d6f53b3145157ff17a658a759da
Jack Hodgson <hodgsonjack@hotmail.com>
2019-04-29 22:59:59 +0100

    require MANAGE_GUILD to use announce and send a message if no args have been provided or no title has been set

--- commands/Administration/announce.js
+++ commands/Administration/announce.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(Client, message, args) => {
-    let deliminator = args.join(" ").split("\"") // title will embedded in quotes
+    message.delete()
 
+    if (!message.member.hasPermission("MANAGE_GUILD")) return message.author.send("You cannot use that command.")
+    if (!args[0]) return message.member.send("You haven't supplied any message to announce.")
+
+    let deliminator = args.join(" ").split("\"") // title will embedded in quotes
     if (!args.join(" ").includes("\"")) { // if there isn't a title set using "title"
         let body = args.join(" ")
         const embed = new Discord.RichEmbed()
@@ -12,6 +16,7 @@ module.exports.run = async(Client, message, args) => {
         .setTimestamp()
 
         message.channel.send(embed)
+        message.author.send(`You sent an announcement without setting a title. To set a title embed the title within quotes. For example:\n${prefix}announce "Title" The other important stuff...`)
     } else { // if there is a title set using "title"
         let title = deliminator[1].replace("\"", "")
         let body = args.join(" ").replace(`"${title}"`, "") // replace the title with an empty string...
