1c5b0b8d5ef4eb2b25e9ecc678a7028044fbda67
Jack Hodgson <hodgsonjack@hotmail.com>
2019-05-05 18:32:57 +0100

    MISC: add some useful comments

--- messageHandler.js
+++ messageHandler.js
@@ -3,11 +3,12 @@ Client.on("message", async message => {
 
     if(message.channel.type === "dm") return; // ignore dms
 
-	// if (!message.member.hasPermission("MANAGE_GUILD")) return;
-    const messageArray = message.content.split(" ");
-    const cmd = messageArray[0];
-    const args = messageArray.slice(1);
-    if (!message.content.startsWith(prefix)) return;
+    const messageArray = message.content.split(" "); // turn args into an array
+    const cmd = messageArray[0]; // this is the command
+    const args = messageArray.slice(1); // this is args minus the prefix and the command
+
+    if (!message.content.startsWith(prefix)) return; // if args doesn't start with the prefix, do nothing
+    
     let commandfile = Client.commands.get(cmd.slice(prefix.length));
     if (commandfile) {
         let embed = new Discord.RichEmbed()
