e0da67160c88624074ff555e7896e9540414b3f4
Jack Hodgson <hodgsonjack@hotmail.com>
2019-05-26 23:31:33 +0100

    DEV: improvements to exec command, supports codeblocks

--- commands/Development/execute.js
+++ commands/Development/execute.js
@@ -4,7 +4,12 @@ module.exports.run = async(Client, message, args) => {
     if(!message.member.hasPermission("ADMINISTRATOR")) { return message.reply('you cannot use that command.').then(() => console.log(`${message.author} tried executing .exec`))}
     if(!args[0]) return;
     try {
-        let code = args.join(" ")
+        let code_delim = args.join(" ").split("\`\`\`")
+        code = code_delim[1].replace("js", "")
+        console.log(`
+            code_delim -> ${typeof(code_delim)} | ${typeof(code_delim) == 'object' ? code_delim[1] : code_delim}
+            code -> ${typeof(code)} | ${code}`
+        )
         eval(code)
     } catch(err) {
         const errembed = new Discord.RichEmbed()
