88f9d8a9700e5e087a03ee526d0a5ea7cf237cb3
Jack Hodgson <hodgsonjack@hotmail.com>
2019-05-26 23:35:06 +0100

    FIX: exec will work if code block is not provided.

--- commands/Development/execute.js
+++ commands/Development/execute.js
@@ -4,6 +4,9 @@ module.exports.run = async(Client, message, args) => {
     if(!message.member.hasPermission("ADMINISTRATOR")) { return message.reply('you cannot use that command.').then(() => console.log(`${message.author} tried executing .exec`))}
     if(!args[0]) return;
     try {
+        if (!args.join(" ").includes("\`\`\`")) {
+            return eval(args.join(" "))
+        }
         let code_delim = args.join(" ").split("\`\`\`")
         code = code_delim[1].replace("js", "")
         console.log(`
