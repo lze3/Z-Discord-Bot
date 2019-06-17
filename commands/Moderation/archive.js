df23c692804403521d705f1ebc5a8f6f27e19254
Jack Hodgson <hodgsonjack@hotmail.com>
2019-06-17 02:52:44 +0100

    fix: delete after 20000ms instead of 50000ms

--- commands/Moderation/archive.js
+++ commands/Moderation/archive.js
@@ -4,8 +4,8 @@ module.exports.run = async(Client, message, args) => {
     message.delete()
     const channel = message.mentions.channels.first()
     const archive_category = "548287037418242058"
-    if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("you do not have permission to execute that command.").then(msg => msg.delete(50000))
-    if (!channel) return message.reply("that is not a channel.").then(msg => msg.delete(50000))
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("you do not have permission to execute that command.").then(msg => msg.delete(20000))
+    if (!channel) return message.reply("that is not a channel.").then(msg => msg.delete(20000))
     await message.guild.channels.get(channel.id)
     await message.guild.channels.get(channel.id).setParent(archive_category)
     await message.guild.channels.get(channel.id).lockPermissions()
@@ -18,7 +18,7 @@ module.exports.run = async(Client, message, args) => {
     .setColor('#EA5343')
     channel.send(embed_psa)
 
-    message.channel.send(`:white_check_mark: **Channel #${channel.name} was successfully archived.**`).then(msg => msg.delete(50000))
+    message.channel.send(`:white_check_mark: **Channel #${channel.name} was successfully archived.**`).then(msg => msg.delete(20000))
     
     await message.guild.channels.get(channel.id).setName(`${channel.name}-old`)
 }
